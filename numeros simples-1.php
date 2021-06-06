d4d364d264bc65bd667269e63992c88379bb8af7
PC\Админ <perechoda@gmail.com>
2021-06-06 22:57:47 +0300

    numeros simples-1.php

---
+++ numeros simples-1.php
@@ -0,0 +1,17 @@
+<?php
+       
+define("DIVISION",5);
+    
+$i = 1;
+
+echo "Resultado:\n";
+    
+    while ($i <= 100) {
+    if ($i % 5 == 0) {
+       echo "$i\n";
+       
+       }
+         $i++;
+   }
+     
+?>
